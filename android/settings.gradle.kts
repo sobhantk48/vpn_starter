bb15b4e1b5bfe5e748e8bed289cfd4d695131a5c
root <root@localhost.localdomain>
2026-07-15 23:54:29 +0330

    Fix Gradle repository conflict and upgrade Kotlin

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -20,11 +20,12 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.12.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    // این خط کلید حل معماست: اگر جایی توی پروژه ریپازیتوری تعریف شده باشه، بیلد رو متوقف می‌کنه
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
